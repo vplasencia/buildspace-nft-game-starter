1bde180038406c3ebc82a929f0e5d9fc7f0e3b65
vplasencia <v.pcalana@gmail.com>
2022-02-01 13:37:48 -0500

    Added loaders

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = "0x8Aa23cacfc05480628a3FBd53C809b7c32cAaAEb";
+
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
